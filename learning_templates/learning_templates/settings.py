68cfeab1ab7b11e1bbb248193f8e6c02b24b8ff1
Richard 艾瑞森 Ericson <43021160+VBoB13@users.noreply.github.com>
2020-05-11 14:58:19 +0800

    Update settings.py

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'n!zup+_+v@$e&vfow^d=w3)vl^_aaewd*f@h4&s@vcyk^j4=(h'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [vbob.pythonanywhere.com]
 
 
 # Application definition
